4e86f4cd4729640d7d5bce93712e1ce448101acf
atsuo1203 <dondonkitemasu@gmail.com>
2017-09-25 04:38:42 +0900

    model名称をuserに変更

--- API/flaskr/models.py
+++ API/flaskr/models.py
@@ -1,14 +1,14 @@
 from flaskr import db
 
 
-class Entry(db.Model):
-    __tablename__ = 'entries'
+class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
     description = db.Column(db.Text)
 
     def __repr__(self):
-        return '<Entry id={id} name={name!r}>'.format(
+        return '<User id={id} name={name!r}>'.format(
                 id=self.id, name=self.name)
 
 
--- API/flaskr/request.py
+++ API/flaskr/request.py
@@ -1,62 +1,62 @@
 from flask import request, redirect, url_for, make_response, jsonify
 from flaskr import app, db
-from flaskr.models import Entry
+from flaskr.models import User
 
 
 @app.route('/', methods=['GET', 'POST'])
-def show_entries():
+def show_users():
     if request.method == 'POST':
         name = request.data.get('name', '')
         description = request.data.get('description', '')
-        entry = Entry(
+        user = User(
             name=name,
             description=description
         )
-        db.session.add(entry)
+        db.session.add(user)
         db.session.commit()
 
     # if method == get
-    entries = Entry.query.all()
+    users = User.query.all()
     results = []
-    for entry in entries:
+    for user in users:
         result = {
-            "name": entry.name,
-            "description": entry.description
+            "name": user.name,
+            "description": user.description
         }
         results.append(result)
     return make_response(jsonify(results))
 
 
 @app.route('/<int:user_id>', methods=['GET', 'PUT', 'DELETE'])
-def add_entry():
-    entry_id = Entry.query.count() + 1
-    entry = Entry(
-        name=str(entry_id),
+def add_user():
+    user_id = User.query.count() + 1
+    user = User(
+        name=str(user_id),
         description='description'
     )
-    db.session.add(entry)
+    db.session.add(user)
     db.session.commit()
-    return redirect(url_for('show_entries'))
+    return redirect(url_for('show_users'))
 
 
 @app.route('/add_sample')
-def add_sample_entry():
-    entry_id = Entry.query.count() + 1
-    entry = Entry(
-        name=str(entry_id),
+def add_sample_user():
+    user_id = User.query.count() + 1
+    user = User(
+        name=str(user_id),
         description='description'
     )
-    db.session.add(entry)
+    db.session.add(user)
     db.session.commit()
-    return redirect(url_for('show_entries'))
+    return redirect(url_for('show_users'))
 
 
 @app.route('/delete_all')
-def delete_all_entry():
+def delete_all_user():
 
-    for entry in Entry.query.all():
-        db.session.delete(entry)
+    for user in User.query.all():
+        db.session.delete(user)
         db.session.commit()
 
-    return redirect(url_for('show_entries'))
+    return redirect(url_for('show_users'))
 
