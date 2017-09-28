1d1c6c5fe01736f3e471c7a3f9b2a226b036d0da
atsuo1203 <dondonkitemasu@gmail.com>
2017-09-28 11:52:41 +0900

    ソートをさせた

--- API/flaskr/__init__.py
+++ API/flaskr/__init__.py
@@ -3,6 +3,9 @@ from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object('flaskr.config')
 
+app.config['JSON_AS_ASCII'] = False   # 日本語文字化け対策
+app.config["JSON_SORT_KEYS"] = False  # ソートをそのまま
+
 db = SQLAlchemy(app)
 
 from flaskr import request
